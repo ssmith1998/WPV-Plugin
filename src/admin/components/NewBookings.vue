090ad82f5ef224d912103eaadf4d594476b8efc0
sean smith <smith.sean1998@gmail.com>
2022-06-19 15:30:08 +0100

    added date format function

--- src/admin/components/NewBookings.vue
+++ src/admin/components/NewBookings.vue
@@ -14,8 +14,8 @@
   <tbody v-if="bookings.length > 0">
     <tr v-for="(booking, index) in bookings" :key="index" @click="onRowClick(booking)">
       <th scope="row">{{index + 1}}</th>
-      <td>{{booking.booking_start_date}}</td>
-      <td>{{booking.booking_end_date}}</td>
+      <td>{{onFormatDate(booking.booking_start_date)}}</td>
+      <td>{{onFormatDate(booking.booking_end_date)}}</td>
       <td>{{booking.email}}</td>
       <td>{{booking.booking_name}}</td>
       <td>{{booking.contact_number}}</td>
@@ -46,6 +46,10 @@ methods: {
         this.modalOpen = true;
         this.currentBooking = booking;
     },
+    onFormatDate(date) {
+        const dateToFormat = new Date(date);
+        return dateToFormat.getDate() + "/" + (dateToFormat.getMonth() + 1) + "/" + dateToFormat.getFullYear() + " " + dateToFormat.getHours() + ":" + dateToFormat.getMinutes();
+    }
 },
 }
 </script>
